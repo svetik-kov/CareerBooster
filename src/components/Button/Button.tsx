38e5cff217dc3e6a1a3e64ee18c7aa1d34d4a995
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-05-29 22:56:13 +0300

    Универсальная кнопка (button) with clsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,16 @@
 import { ComponentPropsWithoutRef } from "react"
 import s from "./Button.module.css"
+import clsx from "clsx"
 
 type Props = {
   variant?: "primary" | "secondary" | "outlined"
 } & ComponentPropsWithoutRef<"button">
 
 export const Button = ({ variant = "primary", className, ...rest }: Props) => {
+  return <button className={clsx(s.button, s[variant], className)} {...rest} />
+}
+
+/*export const Button = ({ variant = "primary", className, ...rest }: Props) => {
   return (
     <button
       className={`${s.button}
@@ -17,4 +22,4 @@ export const Button = ({ variant = "primary", className, ...rest }: Props) => {
       {...rest}
     />
   )
-}
+}*/
